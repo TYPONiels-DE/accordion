ee881115bd115c1cf3393fb10bb80c6c6690f526
Niels Langlotz <niels@langlotz-media.de>
2021-06-04 08:57:25 +0200

    fix: Codingstyles Annotations

--- Classes/Hooks/PageLayoutViewDrawItem.php
+++ Classes/Hooks/PageLayoutViewDrawItem.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TN\Accordion\Hooks;
 
 use TYPO3\CMS\Backend\Form\Exception;
@@ -7,19 +8,21 @@ use TYPO3\CMS\Backend\Form\FormDataGroup\TcaDatabaseRecord;
 use TYPO3\CMS\Backend\View\PageLayoutView;
 use TYPO3\CMS\Backend\View\PageLayoutViewDrawItemHookInterface;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
 class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
 {
     /**
-     * @var array
+     * @var array<string,string>
      */
     protected $supportedContentTypes = array (
-  'accordion_accordion' => 'Accordion',
-);
+    'accordion_accordion' => 'Accordion',
+    );
 
     /**
-     * @var StandaloneView
+     * @var StandaloneView|mixed
+     * @TYPO3\CMS\Extbase\Annotation\Inject
      */
     protected $view;
 
@@ -35,14 +38,15 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
      * @param bool $drawItem
      * @param string $headerContent
      * @param string $itemContent
-     * @param array $row
+     * @param array<string> $row
+     * @return void
      */
     public function preProcess(PageLayoutView &$parentObject, &$drawItem, &$headerContent, &$itemContent, array &$row)
     {
         if (!isset($this->supportedContentTypes[$row['CType']])) {
             return;
         }
-        
+
         $formDataGroup = GeneralUtility::makeInstance(TcaDatabaseRecord::class);
         $formDataCompiler = GeneralUtility::makeInstance(FormDataCompiler::class, $formDataGroup);
         $formDataCompilerInput = [
@@ -51,9 +55,14 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
             'vanillaUid' => (int)$row['uid'],
         ];
         try {
+            /** @phpstan-ignore-next-line */
             $result = $formDataCompiler->compile($formDataCompilerInput);
             $processedRow = $this->getProcessedData($result['databaseRow'], $result['processedTca']['columns']);
-            
+
+            if (!isset($this->view) && $this->view !== null) {
+                $this->view = GeneralUtility::makeInstance(StandaloneView::class);
+            }
+
             $this->configureView($result['pageTsConfig'], $row['CType']);
             $this->view->assignMultiple(
                 [
@@ -61,7 +70,6 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
                     'processedRow' => $processedRow,
                 ]
             );
-    
             $itemContent = $this->view->render();
         } catch (Exception $exception) {
             $message = $GLOBALS['BE_USER']->errorMsg;
@@ -71,13 +79,15 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
 
             $itemContent = $message;
         }
-        
+
         $drawItem = false;
     }
 
     /**
-     * @param array $pageTsConfig
+     * @param array<mixed> $pageTsConfig
      * @param string $contentType
+     * @return void
+     *
      */
     protected function configureView(array $pageTsConfig, $contentType)
     {
@@ -89,10 +99,14 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
         list($extensionKey) = explode('_', $contentType, 2);
         $extensionKey .= '.';
         if (!empty($previewConfiguration[$contentType])) {
-            GeneralUtility::deprecationLog('Setting the complete template path with filename is deprecated and will be removed in accordion_export 3.0');
             $templatePath = GeneralUtility::getFileAbsFileName($previewConfiguration[$contentType]);
             $this->view->setTemplatePathAndFilename($templatePath);
         } else {
+
+            if (!isset($this->view) && $this->view == null) {
+                $this->view = GeneralUtility::makeInstance(StandaloneView::class);
+            }
+
             if (!empty($previewConfiguration[$extensionKey]['templateRootPath'])) {
                 $this->view->setTemplateRootPaths([
                     'EXT:accordion/Resources/Private/Backend/Templates/Content/',
@@ -116,9 +130,9 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
     }
 
     /**
-     * @param array $databaseRow
-     * @param array $processedTcaColumns
-     * @return array
+     * @param array<mixed> $databaseRow
+     * @param array<mixed> $processedTcaColumns
+     * @return array<mixed>
      */
     protected function getProcessedData(array $databaseRow, array $processedTcaColumns)
     {
@@ -131,12 +145,14 @@ class PageLayoutViewDrawItem implements PageLayoutViewDrawItemHookInterface
             foreach ($config['children'] as $child) {
                 if (!$child['isInlineChildExpanded']) {
                     $formDataGroup = GeneralUtility::makeInstance(TcaDatabaseRecord::class);
+                    /* @var FormDataCompiler $formDataCompiler */
                     $formDataCompiler = GeneralUtility::makeInstance(FormDataCompiler::class, $formDataGroup);
                     $formDataCompilerInput = [
                         'command' => 'edit',
                         'tableName' => $child['tableName'],
                         'vanillaUid' => $child['vanillaUid'],
                     ];
+                    /** @phpstan-ignore-next-line */
                     $child = $formDataCompiler->compile($formDataCompilerInput);
                 }
                 $processedRow[$field][] = $this->getProcessedData($child['databaseRow'], $child['processedTca']['columns']);
